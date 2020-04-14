c4daa994538a3ed749da669799b62e006834b374
Leonard Kulms <leonard.kulms@icloud.com>
2020-04-14 17:44:33 +0200

    add interactivity

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,48 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:habitly/habit.dart';
+import 'package:habitly/new_habit_route.dart';
 
 
 class Dashboard extends StatefulWidget {
   const Dashboard();
+
 
   @override
   _DashboardState createState() => _DashboardState();
 }
 
 class _DashboardState extends State<Dashboard> {
+  List<Habit> _habits;
 
-  final _habits = <Habit>[];
-  
-  static const _habitNames = <String>[
-    'Yoga',
-    'no meat'
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-    for (var i = 0; i < _habitNames.length; i++) {
-      _habits.add(Habit(
-        name: _habitNames[i],
-        semanticLabel: 'not done'
-      ));
-    }
+  Future _fetchHabitList() async {
+    List<Habit> _habitCall = await habits();
+    setState(() {
+      _habits = _habitCall;
+    });
   }
 
-  Widget _buildHabitWidgets() {
+  @override
+  void initState(){
+    super.initState();
+    _fetchHabitList();
+  }
+
+  Widget _buildHabitWidgets(){
+    _fetchHabitList();
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) => _habits[index],
       itemCount: _habits.length,
     );
   }
 
-
   @override
   Widget build(BuildContext context) {
     
     final listView = Container(
       color: Colors.white,
-      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      padding: EdgeInsets.symmetric(horizontal: 4.0),
       child: _buildHabitWidgets(),
     );
 
@@ -59,9 +57,20 @@ class _DashboardState extends State<Dashboard> {
       backgroundColor: Colors.white,
     );
 
+    final addHabitButton = FloatingActionButton(
+      onPressed: () {
+        Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => NewHabitRoute()),
+        );
+      },
+      child: Icon(Icons.add),
+    );
+
     return Scaffold(
       appBar: appBar,
       body: listView,
+      floatingActionButton: addHabitButton,
     );    
   }
 }
